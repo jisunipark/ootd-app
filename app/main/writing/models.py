abec168158d1603864f54f67dc0f221c2e7608b0
Lisa <jjwwoo28@gmail.com>
2025-02-18 18:14:04 +0900

    feat : models.py/writing

---
+++ app/main/writing/models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    image_url = db.Column(db.String(200), nullable=True)  # 이미지 URL
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    comments = db.relationship('Comment', backref='post', lazy=True)
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Comment {self.content}>'
