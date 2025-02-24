426e4227cb84f27b1039fdcaa9766aa2b29372b4
Jisun Park <jisunpark.t@gmail.com>
2025-02-25 05:09:12 +0900

    feat: 자신의 유저페이지일 경우 mypage로 리다이렉트

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,20 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user, login_required
 from app.models import Writing, User
 
 user = Blueprint("user", __name__)
 
 @user.route('/user/<int:user_id>')
-def user_page(user_id):
+def userpage(user_id):
+    if user_id == current_user.id:
+        return redirect(url_for("user.mypage")) # 자신의 유저페이지일 경우 mypage로 이동
     username = User.query.get(user_id).username
     user_posts = Writing.query.filter(Writing.user_id == user_id).all()
     return render_template("user.html", username=username, writings=user_posts)
 
 @login_required
 @user.route("/mypage")
-def my_page():
+def mypage():
     user_id = current_user.id
     username = current_user.username
     my_posts = Writing.query.filter(Writing.user_id == user_id).all()
     return render_template("user.html", username=username, writings=my_posts)
-
